af8dbf8c4cb83d5600d4e369ae2675c9e08d458d
alegrey91 <ale_grey_91@hotmail.it>
2022-03-11 17:50:31 +0100

    feat: create kubernetes resources from charm

--- src/charm.py
+++ src/charm.py
@@ -13,11 +13,15 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+import traceback
+from glob import glob
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
+from lightkube import Client, codecs
+from lightkube.core.exceptions import ApiError
 
 logger = logging.getLogger(__name__)
 
@@ -27,14 +31,28 @@ class CharmK8SCapsuleCharm(CharmBase):
 
     _stored = StoredState()
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         super().__init__(*args)
+        self._context = {
+            "namespace": self.model.config["namespace"], 
+            "app_name": self.model.config["name"]
+        }
+        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.capsule_pebble_ready, self._on_capsule_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
 
-    def _on_capsule_pebble_ready(self, event):
+    def _on_install(self, _) -> None:
+        """Handle the install event, create Kubernetes resources."""
+        self.unit.status = MaintenanceStatus("creating kubernetes resources")
+        try:
+            self._create_kubernetes_resources()
+        except ApiError:
+            logger.error(traceback.format_exc())
+            self.unit.status = BlockedStatus("kubernetes resource creation failed")
+
+    def _on_capsule_pebble_ready(self, event) -> None:
         """Define and start a workload using the Pebble API.
 
         TEMPLATE-TODO: change this example to suit your needs.
@@ -70,7 +88,27 @@ class CharmK8SCapsuleCharm(CharmBase):
         # https://juju.is/docs/sdk/constructs#heading--statuses
         self.unit.status = ActiveStatus()
 
-    def _on_config_changed(self, _):
+    def _create_kubernetes_resources(self) -> bool:
+        """Iterates over manifests in the templates directory and applies them to the cluster."""
+        client = Client()
+        # create_resources = ["cluster_roles", "config_maps", "secrets", "services"]
+        # for manifest in create_resources:
+        for manifest in glob("src/templates/*.yaml.j2"):
+            # with open(f"src/templates/{manifest}.yaml.j2") as f:
+            with open(manifest) as f:
+                for resource in codecs.load_all_yaml(f, context=self._context):
+                    try:
+                        client.create(resource)
+                    except ApiError as e:
+                        if e.status.code == 409:
+                            logger.info("replacing resource: %s.", str(resource.to_dict()))
+                            client.replace(resource)
+                        else:
+                            logger.debug("failed to create resource: %s.", str(resource.to_dict()))
+                            raise
+        return True
+
+    def _on_config_changed(self, _) -> None:
         """Just an example to show how to deal with changed configuration.
 
         TEMPLATE-TODO: change this example to suit your needs.
@@ -80,12 +118,13 @@ class CharmK8SCapsuleCharm(CharmBase):
 
         Learn more about config at https://juju.is/docs/sdk/config
         """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+#        current = self.config["thing"]
+#        if current not in self._stored.things:
+#            logger.debug("found a new thing: %r", current)
+#            self._stored.things.append(current)
+        return
 
-    def _on_fortune_action(self, event):
+    def _on_fortune_action(self, event) -> None:
         """Just an example to show how to receive actions.
 
         TEMPLATE-TODO: change this example to suit your needs.
