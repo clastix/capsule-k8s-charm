96e71e0bd781bf0b72ea5a88b244d8f41812c1ff
alegrey91 <ale_grey_91@hotmail.it>
2022-03-21 15:28:00 +0100

    feat: add  resource configuration using juju

--- src/charm.py
+++ src/charm.py
@@ -15,105 +15,162 @@ develop a new k8s charm using the Operator Framework:
 import logging
 import traceback
 import lightkube
+from http import HTTPStatus
 
-from importlib.abc import ResourceReader
-from glob import glob
 from os import path
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
 from lightkube import Client, codecs
-from lightkube.generic_resource import create_namespaced_resource
 from lightkube.core.exceptions import ApiError
 
 logger = logging.getLogger(__name__)
+TEMPLATE_DIR = "src/templates/"
 
+_capsule_configuration_crd = None
+def get_capsule_configuration():
+    """Function to retrieve CapsuleConfiguration class."""
+    global _capsule_configuration_crd
+    if _capsule_configuration_crd == None:
+        _capsule_configuration_crd = create_capsule_configuration()
+    return _capsule_configuration_crd
+
+def create_capsule_configuration():
+    with open(TEMPLATE_DIR + "install.yaml.j2") as f:
+        logger.info("collecting manifests.")
+        for resource in codecs.load_all_yaml(f):
+            
+            if resource.kind != "CustomResourceDefinition":
+                continue
+            # List of custom resource definition to be created.
+            if resource.spec.names.kind == "CapsuleConfiguration":
+                return lightkube.generic_resource.create_global_resource(
+                    group=resource.spec.group, 
+                    version=resource.spec.versions[0].name, 
+                    kind=resource.spec.names.kind, 
+                    plural=resource.spec.names.plural,
+                    verbs=None
+                )
 
 class CharmK8SCapsuleCharm(CharmBase):
     """Charm the service."""
 
     _stored = StoredState()
+    client = Client()
 
     def __init__(self, *args) -> None:
         super().__init__(*args)
+
+        # We have to collect all the available configurations 
+        # in order to fill the Jinja2 manifest templates.
         self._context = {
             "namespace": self.model.config["namespace"], 
-            "app_name": self.model.config["name"]
+            "app_name": self.model.config["name"],
+            "user_groups": self.model.config["user-groups"],
+            "force_tenant_prefix": self.model.config["force-tenant-prefix"],
+            "protected_namespace_regex": self.model.config["protected-namespace-regex"],
         }
+
+        # Assign hook functions to events.
         self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.config_changed, self._on_capsule_configuration_changed)
         self._stored.set_default(things=[])
 
     def _on_install(self, _) -> None:
         """Handle the install event, create Kubernetes resources."""
-        self.unit.status = MaintenanceStatus("creating kubernetes resources")
+        self.unit.status = MaintenanceStatus("creating kubernetes resources.")
         try:
             logger.info("create kubernetes resources.")
-            self._create_kubernetes_resources()
+            if self._create_kubernetes_resources():
+                self.unit.status = ActiveStatus("succesfully installed {} charm.".format(self.meta.name))
+            else:
+                self.unit.status = BlockedStatus("{} creation failed.".format(self.meta.name))
         except ApiError:
             logger.error(traceback.format_exc())
-            self.unit.status = BlockedStatus("kubernetes resource creation failed")
+            self.unit.status = BlockedStatus("{} creation failed.".format(self.meta.name))
 
+        return
+
+    def _on_capsule_configuration_changed(self, event) -> None:
+        """Handle the config_changed event for CapsuleConfiguration resource."""
+        self.unit.status = MaintenanceStatus("changing CapsuleConfiguration resource.")
+        capsule_configuration_crd = get_capsule_configuration()
+
+        # In config changed event the resource should already be created, so we can retrieve this.
+        capsule_configuration_res = self.client.get(
+            capsule_configuration_crd,
+            self.model.config["capsule-configuration-name"]
+        )
+
+        # Update resource attributes.
+        capsule_configuration_res.spec["userGroups"] = self.model.config["user-groups"].split(",")
+        capsule_configuration_res.spec["forceTenantPrefix"] = self.model.config["force-tenant-prefix"]
+        capsule_configuration_res.spec["protectedNamespaceRegex"] = self.model.config["protected-namespace-regex"]
+
+        try:
+            logger.info("changing configuration for %s resource.", capsule_configuration_res.kind)
+            self.client.replace(capsule_configuration_res)
+            self.unit.status = ActiveStatus("%s succesfully changed.".format(capsule_configuration_res.kind))
+        except ApiError:
+            logger.debug("failed to update resource %s.", capsule_configuration_res.kind)
+            self.unit.status = BlockedStatus("{} creation failed.".format(capsule_configuration_res.kind))
+            raise
+
+        return
+        
     def _create_kubernetes_resources(self) -> bool:
         """Iterates over manifests in the templates directory and applies them to the cluster."""
-        client = Client()
-        with open("src/templates/install.yaml.j2") as f:
+        with open(TEMPLATE_DIR + "install.yaml.j2") as f:
             logger.info("collecting manifests.")
             for resource in codecs.load_all_yaml(f, context=self._context):
+                logger.info("creating resource %s from manifest.", resource.kind)
                 try:
-                    logger.info("creating resource %s from manifest.", resource.kind)
-                    client.create(resource)
+                    self.client.create(resource)
                     logger.info("resources %s created.", resource.kind)
                 except ApiError as e:
-                    if e.status.code == 409:
+                    if e.status.code == HTTPStatus.CONFLICT:
                         logger.info("replacing resource: %s.", str(resource.to_dict()))
-                        client.replace(resource)
+                        self.client.replace(resource)
                     else:
                         logger.debug("failed to create resource: %s.", str(resource.to_dict()))
                         raise
                 
+                # List of Capsule CRDs to create.
+                if resource.kind == "CustomResourceDefinition":
+                    if resource.spec.names.kind == "CapsuleConfiguration":
+                        capsule_configuration_crd = get_capsule_configuration()
+                        self._create_custom_resource(capsule_configuration_crd, resource)
+
+        return True
+
+    def _create_custom_resource(self, crd, resource) -> None:
+        """Create a custom resource from a CRD."""
+        if not path.exists(TEMPLATE_DIR + "install-{}.yaml.j2".format(resource.spec.names.kind)):
+            logger.error("no manifest file for resource %s has been found.", resource.spec.names.kind)
+            raise
+
+        with open(TEMPLATE_DIR + "install-{}.yaml.j2".format(resource.spec.names.kind)) as f_crd:
+
+            # Create resource with updated values (stored in _context)
+            for resource_crd in codecs.load_all_yaml(f_crd, self._context):
+                cr = crd(
+                    kind=resource_crd.kind,
+                    apiVersion=resource.spec.group + "/" + resource.spec.versions[0].name,
+                    spec=resource_crd.spec, 
+                    metadata=resource_crd.metadata
+                )
+                logger.info("creating %s resource.", resource_crd.kind)
                 try:
-                    if resource.kind == "CustomResourceDefinition" and path.exists("src/templates/install-{}.yaml.j2".format(resource.spec.names.kind)):
-                        with open("src/templates/install-{}.yaml.j2".format(resource.spec.names.kind)) as f_crd:
-                            logger.info("creating CRD %s for %s.", resource.kind, resource.spec.names.kind)
-                            CRD = lightkube.generic_resource.create_global_resource(
-                                group=resource.spec.group, 
-                                version=resource.spec.versions[0].name, 
-                                kind=resource.spec.names.kind, 
-                                plural=resource.spec.names.plural,
-                                verbs=None
-                            )
-                            for resource_crd in codecs.load_all_yaml(f_crd):
-                                crd = CRD(
-                                    kind=resource_crd.kind,
-                                    apiVersion=resource.spec.group + "/" + resource.spec.versions[0].name,
-                                    spec=resource_crd.spec, 
-                                    metadata=resource_crd.metadata
-                                )
-                                logger.info("creating %s resource.", resource_crd.kind)
-                                client.wait(resource.__class__, name=resource.metadata.name, for_conditions=['Established'])
-                                client.create(crd)
+                    self.client.create(cr)
                 except ApiError as e:
-                    if e.status.code == 409:
+                    if e.status.code == HTTPStatus.CONFLICT:
                         logger.info("replacing resource: %s.", str(resource.to_dict()))
-                        client.replace(resource)
+                        self.client.replace(resource)
                     else:
                         logger.debug("failed to create resource: %s.", str(resource.to_dict()))
                         raise
 
-        return True
-
-    def _on_config_changed(self, _) -> None:
-        """Just an example to show how to deal with changed configuration.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
         return
 
 
