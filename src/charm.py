b4a99bbd28b97b7b864ea81b5b93413c839fb9ff
alegrey91 <ale_grey_91@hotmail.it>
2022-04-04 11:08:10 +0200

    fix: change code for lint and integration tests

--- src/charm.py
+++ src/charm.py
@@ -14,45 +14,51 @@ develop a new k8s charm using the Operator Framework:
 
 import logging
 import traceback
+from http import HTTPStatus
+from os import path
+
 import lightkube
 import yaml
-from http import HTTPStatus
-
-from os import path
+from lightkube import Client, codecs
+from lightkube.core.exceptions import ApiError
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
-from lightkube import Client, codecs
-from lightkube.core.exceptions import ApiError
 
 logger = logging.getLogger(__name__)
 TEMPLATE_DIR = "src/templates/"
 
-_capsule_configuration_crd = None
+CAPSULE_CONFIGURATION_CRD = None
+
+
 def get_capsule_configuration():
     """Function to retrieve CapsuleConfiguration class."""
-    global _capsule_configuration_crd
-    if _capsule_configuration_crd == None:
-        _capsule_configuration_crd = create_capsule_configuration()
-    return _capsule_configuration_crd
+    # pylint: disable=w0603
+    global CAPSULE_CONFIGURATION_CRD
+    if CAPSULE_CONFIGURATION_CRD is None:
+        CAPSULE_CONFIGURATION_CRD = create_capsule_configuration()
+    return CAPSULE_CONFIGURATION_CRD
 
+
+# pylint: disable=R1710
 def create_capsule_configuration():
-    with open(TEMPLATE_DIR + "install.yaml.j2") as f:
+    """Create CapsuleConfiguration class."""
+    with open(TEMPLATE_DIR + "install.yaml.j2", encoding="utf-8") as cc_file:
         logger.info("collecting manifests.")
-        for resource in codecs.load_all_yaml(f):
-            
+        for resource in codecs.load_all_yaml(cc_file):
             if resource.kind != "CustomResourceDefinition":
                 continue
             # List of custom resource definition to be created.
             if resource.spec.names.kind == "CapsuleConfiguration":
                 return lightkube.generic_resource.create_global_resource(
-                    group=resource.spec.group, 
-                    version=resource.spec.versions[0].name, 
-                    kind=resource.spec.names.kind, 
+                    group=resource.spec.group,
+                    version=resource.spec.versions[0].name,
+                    kind=resource.spec.names.kind,
                     plural=resource.spec.names.plural,
-                    verbs=None
+                    verbs=None,
                 )
+
 
 class CharmK8SCapsuleCharm(CharmBase):
     """Charm the service."""
@@ -63,15 +69,15 @@ class CharmK8SCapsuleCharm(CharmBase):
     def __init__(self, *args) -> None:
         super().__init__(*args)
 
-        # Retrieve capsule container image from charm 
+        # Retrieve capsule container image from charm
         capsule_image = self.get_container_image(image_name="capsule-image")
         if capsule_image == "" or capsule_image is None:
             raise Exception("No container image found.")
 
-        # We have to collect all the available configurations 
+        # We have to collect all the available configurations
         # in order to fill the Jinja2 manifest templates.
         self._context = {
-            "namespace": self.model.config["namespace"], 
+            "namespace": self.model.config["namespace"],
             "app_name": self.model.config["name"],
             "capsule_image": capsule_image,
             "user_groups": self.model.config["user-groups"],
@@ -85,9 +91,9 @@ class CharmK8SCapsuleCharm(CharmBase):
         self._stored.set_default(things=[])
 
     def get_container_image(self, image_name: str) -> str:
-        """Retrieve capsule contianer image internally from charm."""
+        """Retrieve capsule container image internally from charm."""
         container_info_path = self.model.resources.fetch(image_name)
-        with open(container_info_path) as cip:
+        with open(container_info_path, encoding="utf-8") as cip:
             return yaml.safe_load(cip)["registrypath"]
 
     def _on_install(self, _) -> None:
@@ -96,15 +102,14 @@ class CharmK8SCapsuleCharm(CharmBase):
         try:
             logger.info("create kubernetes resources.")
             if self._create_kubernetes_resources():
-                self.unit.status = ActiveStatus("succesfully installed {} charm.".format(self.meta.name))
+                self.unit.status = ActiveStatus(f"successfully installed {self.meta.name} charm.")
             else:
-                self.unit.status = BlockedStatus("{} creation failed.".format(self.meta.name))
+                self.unit.status = BlockedStatus(f"{self.meta.name} creation failed.")
         except ApiError:
             logger.error(traceback.format_exc())
-            self.unit.status = BlockedStatus("{} creation failed.".format(self.meta.name))
+            self.unit.status = BlockedStatus(f"{self.meta.name} creation failed.")
 
-        return
-
+    # pylint: disable=W0613
     def _on_capsule_configuration_changed(self, event) -> None:
         """Handle the config_changed event for CapsuleConfiguration resource."""
         self.unit.status = MaintenanceStatus("changing CapsuleConfiguration resource.")
@@ -112,43 +117,49 @@ class CharmK8SCapsuleCharm(CharmBase):
 
         # In config changed event the resource should already be created, so we can retrieve this.
         capsule_configuration_res = self.client.get(
-            capsule_configuration_crd,
-            self.model.config["capsule-configuration-name"]
+            capsule_configuration_crd, self.model.config["capsule-configuration-name"]
         )
 
         # Update resource attributes.
+        # pylint: disable=line-too-long
         capsule_configuration_res.spec["userGroups"] = self.model.config["user-groups"].split(",")
-        capsule_configuration_res.spec["forceTenantPrefix"] = self.model.config["force-tenant-prefix"]
-        capsule_configuration_res.spec["protectedNamespaceRegex"] = self.model.config["protected-namespace-regex"]
+        # pylint: disable=line-too-long
+        capsule_configuration_res.spec["forceTenantPrefix"] = self.model.config[
+            "force-tenant-prefix"
+        ]
+        # pylint: disable=line-too-long
+        capsule_configuration_res.spec["protectedNamespaceRegex"] = self.model.config[
+            "protected-namespace-regex"
+        ]
 
         try:
             logger.info("changing configuration for %s resource.", capsule_configuration_res.kind)
             self.client.replace(capsule_configuration_res)
-            self.unit.status = ActiveStatus("%s succesfully changed.".format(capsule_configuration_res.kind))
+            self.unit.status = ActiveStatus(
+                f"{capsule_configuration_res.kind} successfully changed."
+            )
         except ApiError:
-            logger.debug("failed to update resource %s.", capsule_configuration_res.kind)
-            self.unit.status = BlockedStatus("{} creation failed.".format(capsule_configuration_res.kind))
+            logger.debug("failed to update resource %s", capsule_configuration_res.kind)
+            self.unit.status = BlockedStatus(f"{capsule_configuration_res.kind} creation failed.")
             raise
 
-        return
-        
     def _create_kubernetes_resources(self) -> bool:
         """Iterates over manifests in the templates directory and applies them to the cluster."""
-        with open(TEMPLATE_DIR + "install.yaml.j2") as f:
+        with open(TEMPLATE_DIR + "install.yaml.j2", encoding="utf-8") as kube_manifests:
             logger.info("collecting manifests.")
-            for resource in codecs.load_all_yaml(f, context=self._context):
+            for resource in codecs.load_all_yaml(kube_manifests, context=self._context):
                 logger.info("creating resource %s from manifest.", resource.kind)
                 try:
                     self.client.create(resource)
                     logger.info("resources %s created.", resource.kind)
-                except ApiError as e:
-                    if e.status.code == HTTPStatus.CONFLICT:
+                except ApiError as err:
+                    if err.status.code == HTTPStatus.CONFLICT:
                         logger.info("replacing resource: %s.", str(resource.to_dict()))
                         self.client.replace(resource)
                     else:
                         logger.debug("failed to create resource: %s.", str(resource.to_dict()))
                         raise
-                
+
                 # List of Capsule CRDs to create.
                 if resource.kind == "CustomResourceDefinition":
                     if resource.spec.names.kind == "CapsuleConfiguration":
@@ -157,34 +168,39 @@ class CharmK8SCapsuleCharm(CharmBase):
 
         return True
 
-    def _create_custom_resource(self, crd, resource) -> None:
+    def _create_custom_resource(self, crd, resource) -> bool:
         """Create a custom resource from a CRD."""
-        if not path.exists(TEMPLATE_DIR + "install-{}.yaml.j2".format(resource.spec.names.kind)):
-            logger.error("no manifest file for resource %s has been found.", resource.spec.names.kind)
-            raise
+        if not path.exists(TEMPLATE_DIR + f"install-{resource.spec.names.kind}.yaml.j2"):
+            logger.error(
+                "no manifest file for resource %s has been found.", resource.spec.names.kind
+            )
+            return False
 
-        with open(TEMPLATE_DIR + "install-{}.yaml.j2".format(resource.spec.names.kind)) as f_crd:
+        # pylint: disable=line-too-long
+        with open(
+            TEMPLATE_DIR + f"install-{resource.spec.names.kind}.yaml.j2", encoding="utf-8"
+        ) as f_crd:
 
             # Create resource with updated values (stored in _context)
             for resource_crd in codecs.load_all_yaml(f_crd, self._context):
-                cr = crd(
+                custom_res = crd(
                     kind=resource_crd.kind,
                     apiVersion=resource.spec.group + "/" + resource.spec.versions[0].name,
-                    spec=resource_crd.spec, 
-                    metadata=resource_crd.metadata
+                    spec=resource_crd.spec,
+                    metadata=resource_crd.metadata,
                 )
                 logger.info("creating %s resource.", resource_crd.kind)
                 try:
-                    self.client.create(cr)
-                except ApiError as e:
-                    if e.status.code == HTTPStatus.CONFLICT:
+                    self.client.create(custom_res)
+                except ApiError as err:
+                    if err.status.code == HTTPStatus.CONFLICT:
                         logger.info("replacing resource: %s.", str(resource.to_dict()))
                         self.client.replace(resource)
                     else:
                         logger.debug("failed to create resource: %s.", str(resource.to_dict()))
                         raise
 
-        return
+        return True
 
 
 if __name__ == "__main__":
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -4,49 +4,39 @@
 
 
 import logging
-from os import name
-import yaml
-import pytest
-
 from pathlib import Path
-from pytest_operator.plugin import OpsTest
+
+import pytest
+import yaml
 from lightkube import Client
-from lightkube.resources.core_v1 import (
-    Secret, 
-    Service, 
-)
-from lightkube.resources.rbac_authorization_v1 import (
-    ClusterRoleBinding,
-)
 from lightkube.resources.admissionregistration_v1 import (
     MutatingWebhookConfiguration,
     ValidatingWebhookConfiguration,
 )
-from lightkube.resources.apiextensions_v1 import (
-    CustomResourceDefinition
-)
+from lightkube.resources.apiextensions_v1 import CustomResourceDefinition
+from lightkube.resources.core_v1 import Secret, Service
+from lightkube.resources.rbac_authorization_v1 import ClusterRoleBinding
+from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 
+
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
-    Assert on the unit status before any relations/configurations take place.
-    """
-    # build and deploy charm from local source folder
+    """Build the charm-under-test and deploy it together with related charms."""
     charm = await ops_test.build_charm(".")
     resources = {"capsule-image": METADATA["resources"]["capsule-image"]["upstream-source"]}
     await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME, trust=True)
-    
+
     # issuing dummy update_status just to trigger an event
     await ops_test.model.set_config({"update-status-hook-interval": "10s"})
-    
+
     await ops_test.model.wait_for_idle(apps=[APP_NAME], status="active", timeout=1000)
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
-    
+
     # effectively disable the update status from firing
     await ops_test.model.set_config({"update-status-hook-interval": "60m"})
 
@@ -54,7 +44,6 @@ async def test_build_and_deploy(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_kubernetes_resources_created(ops_test: OpsTest):
     """Test if kubernetes resources have been created properly."""
-
     client = Client()
 
     # If any of these fail, an exception is raised and the test will fail
@@ -64,6 +53,16 @@ async def test_kubernetes_resources_created(ops_test: OpsTest):
     client.get(Secret, name="capsule-ca", namespace=ops_test.model_name)
     client.get(Secret, name="capsule-tls", namespace=ops_test.model_name)
     client.get(Service, name="capsule-webhook-service", namespace=ops_test.model_name)
-    client.get(Service, name="capsule-controller-manager-metrics-service", namespace=ops_test.model_name)
-    client.get(MutatingWebhookConfiguration, name="capsule-mutating-webhook-configuration", namespace=ops_test.model_name)
-    client.get(ValidatingWebhookConfiguration, name="capsule-validating-webhook-configuration", namespace=ops_test.model_name)
+    client.get(
+        Service, name="capsule-controller-manager-metrics-service", namespace=ops_test.model_name
+    )
+    client.get(
+        MutatingWebhookConfiguration,
+        name="capsule-mutating-webhook-configuration",
+        namespace=ops_test.model_name,
+    )
+    client.get(
+        ValidatingWebhookConfiguration,
+        name="capsule-validating-webhook-configuration",
+        namespace=ops_test.model_name,
+    )
